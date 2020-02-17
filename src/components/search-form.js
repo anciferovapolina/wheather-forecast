28aa3b4c43e30e761f564add31afa89e24ef6081
Polina <p.antsyferava@godeltech.com>
2020-02-17 17:32:02 +0300

    commit

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -1,24 +1,59 @@
 import React from 'react';
 import WeatherInfo from './weather-info';
 
+const apiKey = "8ddceeacaf8b95fe943c88fc8389dee0";
+
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       valueCity: '',
-      valueCountry: ''
+      valueCountry: '',
+      resultObj: null
     };
   }
+
+  handleChange(value, name) {
+    this.setState({
+      [name]: value
+    });
+  }
+
+   handleSubmit(event) {
+    event.preventDefault();
+    fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${this.state.valueCity},${this.state.valueCountry}&appid=${apiKey}&units=metric`)
+      .then(
+        response  => response.json()
+      )
+      .then(
+        response => this.setState({
+          resultObj: response
+        })
+      )
+      .catch(
+        error => console.log(error)
+      );
+  }
+
   render() {
     return (
       <div className='col-xs-7 form-container'>
-        <form>
-          <input name='city' type='text' placeholder='city'/>
-          <input name='country' type='text' placeholder='country'/>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input name='valueCity'
+                 type='text'
+                 placeholder='city'
+                 value={this.state.valueCity}
+                 onChange={(event) => this.handleChange(event.target.value, event.target.name)}/>
+          <input name='valueCountry'
+                 type='text'
+                 placeholder='country'
+                 value={this.state.valueCountry}
+                 onChange={(event) => this.handleChange(event.target.value, event.target.name)}/>
           <button type='submit'>Get Weather</button>
         </form>
-
-        <WeatherInfo/>
+        <p style={{'color': 'white'}}>form: {this.state.valueCity} {this.state.valueCountry}</p>
+        <WeatherInfo resultObj={this.state.resultObj}/>
       </div>
     );
   }
--- src/components/weather-info.js
+++ src/components/weather-info.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 class WeatherInfo extends React.Component {
   render() {
+    console.log('this.props.resultObj: ', this.props.resultObj);
     return (
       <div className='weather__info'>
-        <p className='weather__key'>p<span className='weather__value'>p</span></p>
+        <p className='weather__key'>p<span className='weather__value'>{this.props.resultObj?.name}</span></p>
       </div>
     )
   }
